2af03ce254700aafc47db347e339bff396981d6c
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-07-05 09:34:52 +0200

    Populate TemperatureException

--- src/ConcetrationException.java
+++ src/ConcetrationException.java
@@ -1,2 +1,13 @@
 public class ConcetrationException  extends Exception{
+    // props
+    int c;
+
+    public ConcetrationException(int t, String msg) {
+        super(msg);
+        this.c = t;
+    }
+
+    public int getT() {
+        return c;
+    }
 }
